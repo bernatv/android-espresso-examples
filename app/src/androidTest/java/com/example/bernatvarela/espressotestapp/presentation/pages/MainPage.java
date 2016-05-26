b702b1e81ed6bf1d97187584b745a9134c5483b4
Bernat Varela <bernat.varela@redbooth.com>
2016-05-26 12:55:06 +0200

    FIX added first page object

---
+++ app/src/androidTest/java/com/example/bernatvarela/espressotestapp/presentation/pages/MainPage.java
@@ -0,0 +1,20 @@
+package com.example.bernatvarela.espressotestapp.presentation.pages;
+
+import com.example.bernatvarela.espressotestapp.R;
+
+public class MainPage {
+    private static final String movie = "Movie";
+    private static final int list = R.id.mainListView;
+
+    public MainPage() {
+
+    }
+
+    public int getList() {
+        return list;
+    }
+
+    public String getMovie() {
+        return movie;
+    }
+}
--- app/src/androidTest/java/com/example/bernatvarela/espressotestapp/presentation/tests/MainActivityTest.java
+++ app/src/androidTest/java/com/example/bernatvarela/espressotestapp/presentation/tests/MainActivityTest.java
@@ -1,12 +1,19 @@
 package com.example.bernatvarela.espressotestapp.presentation.tests;
 
+import com.example.bernatvarela.espressotestapp.presentation.pages.MainPage;
+
 import org.junit.Test;
 
 public class MainActivityTest extends BaseTest {
-    public static final String MOVIE = "Movie";
+    private MainPage mainPage;
+
+    public MainActivityTest() {
+        mainPage = new MainPage();
+    }
 
     @Test
     public void shouldBeAbleToRunTheTest() {
-        espresso.textDisplayed(MOVIE);
+        espresso.textDisplayed(mainPage.getMovie());
+        espresso.idDisplayed(mainPage.getList());
     }
 }
